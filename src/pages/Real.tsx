e0406a412fc6ca415fa5a1a4d0d6405dfe39f811
thwlckd <thwlckd@naver.com>
2024-05-01 17:23:30 +0900

    Feat: Real API를 이용한 유저 정보 받는 작업 - jsonplaceholder api 사용

--- src/pages/Real.tsx
+++ src/pages/Real.tsx
@@ -1,5 +1,48 @@
+import { useEffect, useState } from 'react';
+
+interface User {
+  id: number;
+  name: string;
+  email: string;
+}
+
 const Real = () => {
-  return <section>Real</section>;
+  const [users, setUsers] = useState<User[]>();
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users');
+
+        setUsers(await response.json());
+      } catch (error) {
+        if (error instanceof Error) {
+          alert(error.message);
+        }
+      }
+    };
+
+    fetchUser();
+  }, []);
+
+  return (
+    <section>
+      <p className="text-2xl mb-4">REAL API TEST</p>
+      {users && (
+        <ul className="flex flex-col gap-4">
+          {users.map(({ id, name, email }) => {
+            return (
+              <li key={id}>
+                <div>id: {id}</div>
+                <div>name: {name}</div>
+                <div>email: {email}</div>
+              </li>
+            );
+          })}
+        </ul>
+      )}
+    </section>
+  );
 };
 
 export default Real;
