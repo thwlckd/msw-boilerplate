82fe46251562df51e6f114c0fddae93f32b1a4f3
thwlckd <thwlckd@naver.com>
2024-05-01 21:26:06 +0900

    Fix: /fake 페이지 리로드 시 발생하는 mock api error 해결 - https://mswjs.io/docs/integrations/browser#conditionally-enable-mocking

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import { worker } from './mocks/browser.ts';
 import './index.css';
 
-worker.start({ onUnhandledRequest: 'bypass' });
+async function enableMocking() {
+  if (import.meta.env.VITE_ENV !== 'development') {
+    return;
+  }
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  const { worker } = await import('./mocks/browser');
+
+  return worker.start({ onUnhandledRequest: 'bypass' });
+}
+
+enableMocking().then(() =>
+  ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  ),
 );
